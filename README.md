ea2f843b5ddc9d92eea3440aeed2fccd68a2dd42
WangDanmeng <WangDanmeng98@163.com>
2022-12-08 17:41:55 +0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# 以开发模式运行
+
+```
+yarn dev
+# 或
+npm run dev
+```
+
+# 以生成模式运行
+
+```
+yarn build
+# 或
+npm run build
+```
---
+++ src/main.js
@@ -0,0 +1,65 @@
+const $siteList = $('.site-list')
+const $lastBox = $siteList.find('li.last')
+let liObject = JSON.parse(localStorage.getItem('li'))
+const hashMap = liObject || [
+  { logo: 'G', url: 'https://google.com' },
+  { logo: 'B', url: 'https://bilibili.com' },
+]
+const removePrefix = (url) => {
+  return url.replace('https://', '').replace('www.', '').replace(/\/.*/, '')
+}
+
+function render() {
+  $siteList.find('li:not(li.last)').remove()
+  hashMap.forEach((node, index) => {
+    const $li = $(`
+      <li>
+        <div class="site">
+          <div class="logo">${node.logo[0]}</div>
+          <div class="link">${removePrefix(node.url)}</div>
+          <div class="close">
+            <svg class="icon">
+              <use xlink:href="#icon-del"></use>
+            </svg>
+          </div>
+        </div>
+      </li>
+    `).insertBefore($lastBox)
+    $li.on('click', () => {
+      window.open(node.url)
+    })
+    // 删除收藏
+    $li.on('click', '.close', (e) => {
+      e.stopPropagation()
+      hashMap.splice(index, 1)
+      render()
+    })
+  })
+}
+render()
+
+$('.addButton').on('click', () => {
+  let url = window.prompt('请输入要添加的网址链接')
+  if (url.trim() === '') {
+    alert('请输入有效网址')
+    return
+  }
+  if (url.indexOf('https://') !== 0) {
+    url = 'https://' + url
+  }
+  hashMap.push({
+    logo: removePrefix(url)[0],
+    url: url,
+  })
+  $siteList.find('li:not(.last)').remove()
+  render()
+})
+
+// 页面退出或刷新前localStorage保存收藏
+onunload = () => {
+  localStorage.setItem('li', JSON.stringify(hashMap))
+}
+
+$(document).on('keypress', (e) => {
+  console.log(e.keyCode === 13)
+})
